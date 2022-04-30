7e92f9e86600facd7596222a6e378efcd28a8845
Rimsa <roberts.rimsa@neotech.lv>
2022-04-30 11:38:14 +0300

    Initial commit

---
+++ src/main/java/com/example/testtaskbankingcore/TestTaskBankingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtaskbankingcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskBankingCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskBankingCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtaskbankingcore/TestTaskBankingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtaskbankingcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskBankingCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
